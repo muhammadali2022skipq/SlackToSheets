95f9272b7f814f1edb9b07c274aa0d7e04851106
muhammadali2022skipq <muhammadali.khan.skipq@gmail.com>
2022-05-01 23:32:02 +0500

    Added modules to local slack to sheet folder

--- SlackToSheet/pipeline_stack.py
+++ SlackToSheet/pipeline_stack.py
@@ -30,7 +30,6 @@ class SlackToSheetStackPipelineStack(Stack):
             source=source,
             commands_list=[
                 "pip install -r requirements.txt",
-                "pip install google-api-python-client google-auth-httplib2 google-auth-oauthlib --target=SlackToSheet/",
                 "npm install -g aws-cdk",
                 "cdk synth",
             ],
