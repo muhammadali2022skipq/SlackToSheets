6f2713688ce21dff55d0dfadb06bb25afc12d9b8
muhammadali2022skipq <muhammadali.khan.skipq@gmail.com>
2022-05-02 16:31:59 +0500

    Updated Consumer Lambda

--- SlackToSheet/app_stack.py
+++ SlackToSheet/app_stack.py
@@ -19,6 +19,7 @@ class SlackToSheetStack(Stack):
         lamdaRoles = self.create_lamba_role()
         ProducerLambda_Function = self.create_lambda(
             "SlackToSheets_ProducerLambda",
+            "pip install --upgrade google-api-python-client oauth2client --target=resources/"
             './resources',
             'ProducerLambda.handler_name',
             lamdaRoles
--- resources/ConsumerLambda.py
+++ resources/ConsumerLambda.py
@@ -3,7 +3,6 @@ import json
 import boto3 as b3_
 from custom_encoder import CustomEncoder
 from oauth2client.service_account import ServiceAccountCredentials
-from google.oauth2.credentials import Credentials
 from googleapiclient.discovery import build
 
 logger = logging.getLogger()
@@ -11,32 +10,36 @@ logger.setLevel(logging.INFO)
 
 
 def handler_name(event, context):
+    spread_id = "1NLGfAEevoz1zrkveMw7z8XCYMN1C7IbuaY09Vb2mdf4"
+
     logger.info(event)
     scopes = ["https://www.googleapis.com/auth/drive"]
     client = b3_.client('secretsmanager')
-
-   # GoogleDriveAPIURL = "https://www.googleapis.com/drive/v3/files?key={Google_Drive_API_Key}".format(
-    #    Google_Drive_API_Key=GoogleDriveAPIKey)
-
-    # headers = {
-    #    "Authorization": "Bearer MYREALLYLONGTOKENIGOT"
-    # }
-
     secret_key = client.get_secret_value(
-        SecretId="google-api-json-key-https",
+        SecretId="google-api-json-key",
     )
     json_keys = json.loads(secret_key['SecretString'])
     logger.info(json_keys)
-    creds = Credentials.from_authorized_user_info(
-        info=json_keys['web'], scopes=scopes)
-
+    creds = ServiceAccountCredentials.from_json_keyfile_dict(
+        keyfile_dict=json_keys, scopes=scopes)
     logger.info("CREDS BELOW")
     logger.info(creds)
-    # creds = ServiceAccountCredentials.from_json_keyfile_dict(
-    #     keyfile_dict=json_keys, scopes=scopes)
-    # sheets = build('sheets', 'v4', credentials=creds, cache_discovery=False)
+
+    sheets = build('sheets', 'v4', credentials=creds, cache_discovery=False)
 
     return buildResponse(200, event)
+
+    # Method to authorize google api using OAuth2.0
+    # creds = Credentials.from_authorized_user_info(
+    #     info=json_keys['web'], scopes=scopes)
+    # secret_key = client.get_secret_value(
+    #     SecretId="GoogleAPIKey",
+    # )
+    # GoogleDriveAPIURL = "https://www.googleapis.com/drive/v3/files?key={Google_Drive_API_Key}".format(
+    #     Google_Drive_API_Key=secret_key)
+    # headers = {
+    #    "Authorization": "Bearer MYREALLYLONGTOKENIGOT"
+    # }
 
 
 def buildResponse(statusCode, body=None):
